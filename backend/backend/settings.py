2006db4be5a045375445837f6cd9e6145775212a
MikyDev <chonakbeatz@gmail.com>
2023-03-26 20:09:11 -0300

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Carpeta.NombreArchivo.NombreFuncion"
+    'ventas',
 ]
 
 MIDDLEWARE = [
