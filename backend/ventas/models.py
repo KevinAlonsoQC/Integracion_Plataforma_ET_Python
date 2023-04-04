ba0e7e068b76d4099088329d57d2ae5fe93c9494
MikyDev <chonakbeatz@gmail.com>
2023-04-04 17:18:48 -0400

    normalización de sql

--- backend/ventas/models.py
+++ backend/ventas/models.py
@@ -83,6 +83,13 @@ class Color(models.Model):
     rgb=models.CharField(max_length=255, verbose_name='Paleta_RGB', null=False)
     hex=models.CharField(max_length=255,verbose_name='Paleta_Hex', null=False)
 
+class Tipo_Cuenta(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre_tipo=models.CharField(nique=True,max_length=255, verbose_name='Tipo_De_Cuenta', null=False)
+    desc=models.CharField(nique=True,max_length=255, verbose_name='Descripcion', null=False)
+    nivel_staff=models.IntegerField(verbose_name='Nivel Staff', null=True, default=0) #ForeignKey 
+
 
 class Empleado(models.Model):
     id = models.AutoField(primary_key=True)
@@ -96,11 +103,29 @@ class Empleado(models.Model):
 
     sexo=models.ForeignKey(Sexo, on_delete=models.CASCADE)
     celular=models.CharField(max_length=9, verbose_name='Celular', null=False) 
-    edad=models.IntegerField(verbose_name='Edad', null=False) 
+    nacimiento=models.DateField(verbose_name="Fecha_Nacimiento", null=False)
 
     comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
+    tipo_cuenta=models.ForeignKey(Tipo_Cuenta, on_delete=models.CASCADE) #ForeignKey
 
-    nivel_staff=models.IntegerField(verbose_name='Nivel Staff', null=True, default=0) #ForeignKey 
+
+class Sucursal(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre_sucursal=models.CharField(nique=True,max_length=255, verbose_name='Color', null=False)
+    telefono=models.CharField(max_length=255,verbose_name='Paleta_Hex', null=False)
+
+    gerente=models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
+
+class Bodega(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre_bodega=models.CharField(nique=True,max_length=255, verbose_name='Color', null=False)
+    telefono=models.CharField(max_length=255,verbose_name='Paleta_Hex', null=False)
+    encargado=models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
+
 
 class Proovedor(models.Model):
     id = models.AutoField(primary_key=True)
@@ -114,7 +139,7 @@ class Proovedor(models.Model):
     tipo_proovedor=models.ForeignKey(Tipo_Proovedor, on_delete=models.CASCADE, null=True) #ForeignKey
     comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
 
-class Ventas(models.Model):
+class Ventas_Mes(models.Model):
     id = models.AutoField(primary_key=True)
     
     monto_total=models.IntegerField(verbose_name='Dinero_Total_Mes', null=False)
@@ -130,7 +155,7 @@ class Categoria(models.Model):
     tipo_categoria=models.TextField(verbose_name='Tipo_Categoria', null=True)
     desc=models.TextField(verbose_name='Descripcion', null=True)
 
-class Edad(models.Model):
+class Edad_Ropa(models.Model):
     id = models.AutoField(primary_key=True)
 
     rango_edad=models.CharField(verbose_name='Rango_Edad', null=True)
@@ -142,8 +167,7 @@ class Producto(models.Model):
     nombre_producto=models.CharField(max_length=100, verbose_name='Nombre_Producto', null=False)
     categoria_producto=models.ForeignKey(Categoria, on_delete=models.CASCADE) #ForeignKey
     sexo=models.ForeignKey(Sexo, on_delete=models.CASCADE, null=True) #ForeignKey
-    edad=models.ForeignKey(Edad, on_delete=models.CASCADE, null=True) #ForeignKey
-
+    edad_de_ropa=models.ForeignKey(Edad_Ropa, on_delete=models.CASCADE, null=True) #ForeignKey
 
     descripcion=models.TextField(verbose_name='Descripcion', null=True)
     precio=models.IntegerField(verbose_name='Precio', null=False)
@@ -163,8 +187,7 @@ class Clientes(models.Model):
 
     sexo=models.ForeignKey(Sexo, on_delete=models.CASCADE)
     celular=models.CharField(max_length=9, verbose_name='Celular', null=False) 
-    edad=models.IntegerField(verbose_name='Edad', null=False) 
-    
+    nacimiento=models.DateField(verbose_name="Fecha_Nacimiento", null=False)
     comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
 
 class Tipo_Estado(models.Model):
@@ -173,21 +196,30 @@ class Tipo_Estado(models.Model):
     estado=models.CharField(max_length=100, verbose_name='Tipo_Estado', null=False)
     desct=models.CharField(max_length=100, verbose_name='Descripcion', null=False)
 
-class Ordenes(models.Model):
+class Tipo_Impreso(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    tipo=models.CharField(max_length=100, verbose_name='Boleta_Factura', null=False)
+    desct=models.CharField(max_length=100, verbose_name='Descripcion', null=False)
+
+
+class Orden(models.Model):
     id = models.AutoField(primary_key=True)
     numero_orden=models.CharField(max_length=100, verbose_name='Numero_Orden', null=False)
     precio_total=models.CharField(max_length=250, verbose_name='Precio_Total', null=False)
     fecha_orden=models.DateField(auto_now_add=True, verbose_name="Fecha_Orden", null=False)
 
+    comprador=models.ForeignKey(Clientes, on_delete=models.CASCADE)#ForeignKey
+    sucursal=models.ForeignKey(Sucursal, on_delete=models.CASCADE)#ForeignKey
+    vendedor=models.ForeignKey(Empleado, on_delete=models.CASCADE)#ForeignKey
     estado=models.ForeignKey(Tipo_Estado, on_delete=models.CASCADE)  #ForeignKey
-
+    tipo_impreso=models.ForeignKey(Tipo_Impreso, on_delete=models.CASCADE)  #ForeignKey
     tipo_pago=models.ForeignKey(Tipo_Pago, on_delete=models.CASCADE)  #ForeignKey
-    cliente_orden=models.ForeignKey(Clientes, on_delete=models.CASCADE)#ForeignKey
 
 class Detalle_Orden(models.Model):
     id = models.AutoField(primary_key=True)
 
-    numero_orden=models.ForeignKey(Ordenes, on_delete=models.CASCADE)  #ForeignKey
+    numero_orden=models.ForeignKey(Orden, on_delete=models.CASCADE)  #ForeignKey
     producto=models.ForeignKey(Producto, on_delete=models.CASCADE)  #ForeignKey
 
     cantidad=models.IntegerField(verbose_name="Cantidad_Producto", null=False)
@@ -206,3 +238,6 @@ class Inventario(models.Model):
     stock_en_espera=models.IntegerField(verbose_name='Stock_En_Espera', null=False) 
     stock_vendido=models.IntegerField(verbose_name='Stock_Vendido', null=False) 
     stock_devolucion=models.IntegerField(verbose_name='Stock_Devolucion', null=False) 
+
+    bodega=models.ForeignKey(Bodega, on_delete=models.CASCADE)#ForeignKey
+
