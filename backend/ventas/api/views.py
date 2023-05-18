f10ee9b7c21a7e3c19645d8b531f3017f2ef2c50
MikyDev <chonakbeatz@gmail.com>
2023-05-18 19:07:00 -0400

    update:captar post, patch, put y delete en API

--- backend/ventas/api/views.py
+++ backend/ventas/api/views.py
@@ -3,6 +3,10 @@ from django.db import models
 from rest_framework import serializers, viewsets, status, routers
 from django.http import JsonResponse
 
+#ACÁ SOLO MODIFICAR LOS CREATE, DESTROY Y UPDATE DONDE DICE #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+#ACÁ SOLO MODIFICAR LOS CREATE, DESTROY Y UPDATE DONDE DICE #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+#ACÁ SOLO MODIFICAR LOS CREATE, DESTROY Y UPDATE DONDE DICE #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+#ACÁ SOLO MODIFICAR LOS CREATE, DESTROY Y UPDATE DONDE DICE #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
 def create_serializer_and_viewset(modelos):
     class Serializer(serializers.ModelSerializer):
         class Meta:
@@ -18,12 +22,47 @@ def create_serializer_and_viewset(modelos):
             if serializer.is_valid():
                 # Los datos recibidos son válidos, podemos crear un nuevo objeto
                 self.perform_create(serializer)
-                return JsonResponse({'status':status.HTTP_201_CREATED, 'Mensaje':'Ha sido creado...'})
+                print('Objeto creado')
+
+                #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+                return JsonResponse({'Mensaje':'Ha sido creado...'})
             else:
                 # Los datos recibidos no son válidos
-                return JsonResponse({'status': status.HTTP_400_BAD_REQUEST, 'Mensaje':'Datos incorrectos...'})
+                return JsonResponse({'Mensaje':'Datos incorrectos...'})
+
+        def update(self, request, *args, **kwargs):
+            instancia = self.get_object()  # Obtener la instancia que está siendo actualizada
+            # Realizar acciones adicionales aquí antes de la actualización
+
+            # Llamar al método 'update' del padre para realizar la actualización
+            # Esto actualizará los campos en la instancia con los datos proporcionados en la solicitud
+            super().update(request, *args, **kwargs)
+
+            #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+            print('Objeto actualizado')
+
+            return JsonResponse({'Mensaje': 'Actualización exitosa'})
+
+        def destroy(self, request, *args, **kwargs):
+            instancia = self.get_object()  # Obtener la instancia que se eliminará
+            # Realizar acciones adicionales aquí antes de la eliminación
+
+            # Llamar al método 'destroy' del padre para realizar la eliminación
+            super().destroy(request, *args, **kwargs)
+
+            #AGREGAR VALIDACIONES ACÁ O ACTUALIZACIONES,ETC
+            print('Objeto eliminado')
+
+            return JsonResponse({'Mensaje': 'Eliminación exitosa'})
+
     return ViewSet
 
+
+#De acá para abajo no modificar NADA
+#De acá para abajo no modificar NADA
+#De acá para abajo no modificar NADA
+#De acá para abajo no modificar NADA
+#De acá para abajo no modificar NADA
 def get_dynamic_viewsets():
     """
     Esto genera dinámicamente serializers y viewsets de DRF para todos los modelos de Django
@@ -38,33 +77,25 @@ def get_dynamic_viewsets():
         'ContentType',
         'Session',
     ]
-
     viewsets_dict = {}
-
     # Iterar sobre todos los modelos de Django en la aplicación
     for model in apps.get_models():
         # Verificar que el modelo sea una subclase de django.db.models.Model
         if not issubclass(model, models.Model):
             continue
-
         # Excluir ciertos modelos
         if model.__name__ in excluded_models:
             continue
-
         # Generar un nombre de clase para el Viewset
         viewset_name = f'{model.__name__}ViewSet'
-
         # Crear una clase Viewset dinámica para el modelo actual
         viewset_class = create_serializer_and_viewset(model)
-
         # Agregar el viewset generado al diccionario de viewsets
         viewsets_dict[model.__name__.lower()] = viewset_class
-
     return viewsets_dict
 
 # Obtener los viewsets dinámicamente
 dynamic_viewsets = get_dynamic_viewsets()
-
 # Registrar los viewsets en el router
 router = routers.DefaultRouter()
 for model_name, viewset in dynamic_viewsets.items():
