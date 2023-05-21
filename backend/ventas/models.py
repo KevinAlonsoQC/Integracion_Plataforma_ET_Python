2eb4bce6d5abe1b95e6ed0ee67230efbad2d8e89
MikyDev <chonakbeatz@gmail.com>
2023-05-20 21:52:28 -0400

    update:bodega relationship with sucursal

--- backend/ventas/models.py
+++ backend/ventas/models.py
@@ -163,18 +163,6 @@ class Clientes(models.Model):
     def __str__(self):
         return self.nombre+' '+self.apellido
 
-class Sucursal(models.Model):
-    id = models.AutoField(primary_key=True)
-
-    nombre_sucursal=models.CharField(unique=True,max_length=255, verbose_name='Nombre_Sucursal', null=False)
-    telefono=models.CharField(max_length=255,verbose_name='Telefono', null=False)
-
-    gerente=models.ForeignKey(Empleado, on_delete=models.CASCADE)
-    comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
-
-    def __str__(self):
-        return self.nombre_sucursal
-
 class Bodega(models.Model): #16
     id = models.AutoField(primary_key=True)
 
@@ -185,6 +173,19 @@ class Bodega(models.Model): #16
 
     def __str__(self):
         return self.nombre_bodega
+
+class Sucursal(models.Model):
+    id = models.AutoField(primary_key=True)
+
+    nombre_sucursal=models.CharField(unique=True,max_length=255, verbose_name='Nombre_Sucursal', null=False)
+    telefono=models.CharField(max_length=255,verbose_name='Telefono', null=False)
+
+    gerente=models.ForeignKey(Empleado, on_delete=models.CASCADE)
+    comuna=models.ForeignKey(Comuna, on_delete=models.CASCADE) #ForeignKey
+    bodega_sucursal=models.ForeignKey(Bodega, on_delete=models.CASCADE) #ForeignKey
+
+    def __str__(self):
+        return self.nombre_sucursal
 
 class Proovedor(models.Model):
     id = models.AutoField(primary_key=True)
